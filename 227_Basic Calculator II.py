42e68019b576fbe4cc3d3de72dd15ac8fdf64858
sgmzhou4 <zhou19951014@gmail.com>
2020-03-15 16:27:18 -0400

    update

---
+++ 227_Basic Calculator II.py
@@ -0,0 +1,53 @@
+'''
+Implement a basic calculator to evaluate a simple expression string.
+
+The expression string contains only non-negative integers, +, -, *, / operators and empty spaces . The integer division should truncate toward zero.
+
+Example 1:
+
+Input: "3+2*2"
+Output: 7
+Example 2:
+
+Input: " 3/2 "
+Output: 1
+Example 3:
+
+Input: " 3+5 / 2 "
+Output: 5
+Note:
+
+You may assume that the given expression is always valid.
+Do not use the eval built-in library function.
+'''
+
+class Solution(object):
+    def calculate(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if not s:
+            return
+        stack = []
+        num = 0
+        sign = '+'
+        for i in range(len(s)):
+            if s[i].isdigit():
+                num = num*10 + ord(s[i]) - ord('0')
+            if (not s[i].isdigit() and not s[i].isspace()) or i == len(s) - 1:
+                if sign == '-':
+                    stack.append(-num)
+                elif sign == '+':
+                    stack.append(num)
+                elif sign == '*':
+                    stack.append(stack.pop()*num)
+                else:
+                    tmp = stack.pop()
+                    if tmp//num < 0 and tmp%num != 0:
+                        stack.append(tmp//num + 1)
+                    else:
+                        stack.append(tmp//num)
+                sign = s[i]
+                num = 0
+        return sum(stack)
---
+++ 240_Search a 2D Matrix II.py
@@ -0,0 +1,43 @@
+'''
+Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+
+Integers in each row are sorted in ascending from left to right.
+Integers in each column are sorted in ascending from top to bottom.
+Example:
+
+Consider the following matrix:
+
+[
+  [1,   4,  7, 11, 15],
+  [2,   5,  8, 12, 19],
+  [3,   6,  9, 16, 22],
+  [10, 13, 14, 17, 24],
+  [18, 21, 23, 26, 30]
+]
+Given target = 5, return true.
+
+Given target = 20, return false.
+'''
+
+# scan from right top to left down
+
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        if not matrix:
+            return False
+        m = len(matrix)
+        n = len(matrix[0])
+        top, right = 0, n-1
+        while top < m and right >= 0:
+            if matrix[top][right] > target:
+                right -= 1
+            elif matrix[top][right] < target:
+                top += 1
+            else:
+                return True
+        return False
